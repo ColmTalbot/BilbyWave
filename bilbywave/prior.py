db1a21f01f5ba18da84cc91d50c6ba1b8f405558
Ethan Payne <ethan.payne@ligo.org>
2020-08-17 10:12:29 +1000

    added analytic spike and slab prior

--- bilbywave/prior.py
+++ bilbywave/prior.py
@@ -1,5 +1,5 @@
-from bilby.core.prior import Prior, ConditionalPowerLaw
-
+from bilby.core.prior import Prior, ConditionalPowerLaw, Uniform
+import numpy as np
 
 class OrderedAmplitude(ConditionalPowerLaw):
     def __init__(self, order, base_name, latex_label, minimum, maximum):
@@ -32,3 +32,87 @@ class OrderedAmplitude(ConditionalPowerLaw):
             if key in instantiation_dict:
                 instantiation_dict[key] = value
         return instantiation_dict
+
+
+class SpikeSlabPrior(Prior):
+    def __init__(self, peak, slab_probability, name=None, latex_label=None,
+                 unit=None, boundary=None, slab_prior=Uniform, **slab_prior_kwargs):
+        """Slab-spike prior. Requires location of the peak and the prior probability of the slab.
+
+        Parameters
+        ----------
+        peak: float
+            Sets the peak of the spike
+        slab_probability: float
+            Sets the probability of the sample being drawn from the slab.
+        name: str
+            See superclass
+        latex_label: str
+            See superclass
+        unit: str
+            See superclass
+        boundary: str
+            See superclass
+        slab_prior_kwargs:
+            Remaining kwargs are the parameters for the slab prior
+        """
+        super(SpikeSlabPrior, self).__init__(name=name, latex_label=latex_label,
+                                       unit=unit,
+                                       boundary=boundary)
+        self.peak = peak
+        self.slab_probability = slab_probability
+        self.slab_prior = slab_prior(**slab_prior_kwargs)
+
+    def rescale(self, val):
+        L_min = self.slab_probability * self.slab_prior.cdf(self.peak)
+        L_max = self.slab_probability * (1 - self.slab_prior.cdf(self.peak))
+
+        if not isinstance(val, float):
+            arr = np.zeros(len(val))
+
+            before_peak = (val < L_min)
+            after_peak = (val > 1-L_max)
+            at_peak = (val > L_min)*(val < 1-L_max)
+
+            arr[before_peak] += self.slab_prior.rescale(val[before_peak]/self.slab_probability)
+            arr[after_peak] += self.slab_prior.rescale((val[after_peak] - (1-self.slab_probability))/self.slab_probability)
+            arr[at_peak] += self.peak * val[at_peak] ** 0
+            return arr
+
+        else:
+            if val < L_min:
+                return self.slab_prior.rescale(val/self.slab_probability)
+            elif val > 1-L_max:
+                return self.slab_prior.rescale((val - (1-self.slab_probability))/self.slab_probability)
+            else:
+                return self.peak * val ** 0
+
+
+    def prob(self, val):
+        if not isinstance(val, float):
+            arr = self.slab_prior.prob(val) * self.slab_probability
+            at_peak = (val==self.peak)
+            arr[at_peak] = np.inf * (1-self.slab_probability)
+
+            return arr
+
+        else:
+            if val == self.peak:
+                return np.inf * (1-self.slab_probability)
+            else:
+                return self.slab_prior.prob(val) * self.slab_probability
+
+
+    def cdf(self, val):
+        if not isinstance(val, float):
+            arr = self.slab_prior.cdf(val) * self.slab_probability
+            after_peak = (val>self.peak)
+            arr[after_peak] = self.slab_prior.cdf(val[after_peak]) * self.slab_probability + (1-self.slab_probability)
+
+            return arr
+
+        else:
+            if val <= self.peak:
+                return self.slab_prior.cdf(val) * self.slab_probability
+            else:
+                return self.slab_prior.cdf(val) * self.slab_probability + (1-self.slab_probability)
