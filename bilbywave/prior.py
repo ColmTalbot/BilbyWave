7a10038727556e1a617455d008d04d3ca396a7e4
Ethan Payne <ethan.payne@ligo.org>
2020-09-11 17:41:58 +1000

    removed generic slab-spike prior - leaving the SpikeSlabPowerLaw, OrderedSpikeSlabAmplitude and OrderedAmplitude priors

--- bilbywave/prior.py
+++ bilbywave/prior.py
@@ -33,62 +33,6 @@ class OrderedAmplitude(ConditionalPowerLaw):
                 instantiation_dict[key] = value
         return instantiation_dict
 
-
-class SpikeSlabPrior(Prior):
-    def __init__(self, peak, slab_probability, name=None, latex_label=None,
-                 unit=None, boundary=None, slab_prior=Uniform, **slab_prior_kwargs):
-        """Slab-spike prior. Requires location of the peak and the prior probability of the slab.
-
-        Parameters
-        ----------
-        peak: float
-            Sets the peak of the spike
-        slab_probability: float
-            Sets the probability of the sample being drawn from the slab.
-        name: str
-            See superclass
-        latex_label: str
-            See superclass
-        unit: str
-            See superclass
-        boundary: str
-            See superclass
-        slab_prior_kwargs:
-            Remaining kwargs are the parameters for the slab prior
-        """
-        super(SpikeSlabPrior, self).__init__(name=name, latex_label=latex_label,
-                                       unit=unit,
-                                       boundary=boundary)
-        self.peak = peak
-        self.slab_probability = slab_probability
-        slab_prior_kwargs['latex_label'] = latex_label
-        self.slab_prior = slab_prior(**slab_prior_kwargs)
-
-    def rescale(self, val):
-        L_min = self.slab_probability * self.slab_prior.cdf(self.peak)
-        L_max = self.slab_probability * (1 - self.slab_prior.cdf(self.peak))
-
-        if not isinstance(val, float):
-            arr = np.zeros(len(val))
-
-            before_peak = (val < L_min)
-            after_peak = (val > 1-L_max)
-            at_peak = (val > L_min)*(val < 1-L_max)
-
-            arr[before_peak] += self.slab_prior.rescale(val[before_peak]/self.slab_probability)
-            arr[after_peak] += self.slab_prior.rescale((val[after_peak] - (1-self.slab_probability))/self.slab_probability)
-            arr[at_peak] += self.peak * val[at_peak] ** 0
-            return arr
-
-        else:
-            if val < L_min:
-                return self.slab_prior.rescale(val/self.slab_probability)
-            elif val > 1-L_max:
-                return self.slab_prior.rescale((val - (1-self.slab_probability))/self.slab_probability)
-            else:
-                return self.peak * val ** 0
-
-
     def prob(self, val):
         if not isinstance(val, float):
             arr = self.slab_prior.prob(val) * self.slab_probability
